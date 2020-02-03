10151f8eb83c46dcd40130fa2d1c9311744d4646
Arafat Tai <tai.arafat.at@gmail.com>
2020-02-03 16:59:04 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+
+console.log(__dirname);
+console.log(__filename);
+console.log();
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)=> {
+    res.render('index')
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About us'
+    })
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is up on port 3000");
+})
