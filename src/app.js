c8e13a2a5241fcf2b8d2c0f233ac07b2c45f994b
Arafat Tai <tai.arafat.at@gmail.com>
2020-02-07 12:22:10 +0530

    added header and footer templates

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 console.log(__dirname);
 console.log(__filename);
 console.log();
 
+//Defines path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
 const app = express()
 
+//Setup handle bar engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res)=> {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather'
+    })
 })
 
 app.get('/about', (req,res) => {
@@ -23,7 +36,8 @@ app.get('/about', (req,res) => {
 
 app.get('/help',(req, res)=>{
     res.render('help', {
-        msg: 'Help!!!!!'
+        msg: 'Help!!!!!',
+        title: 'Help!'
     })
 })
 app.listen(3000, ()=> {
